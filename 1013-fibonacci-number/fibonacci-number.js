1cdf2049d3b59c41338c3765acd1b8e76f7e77be
Mitu kr Rabha <166889598+Mitu03@users.noreply.github.com>
2025-07-29 00:58:31 +0530

    Time: 50 ms (58.46%) | Memory: 53.2 MB (69.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -2,8 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib = function(n, memo= {}) {
+    if(n === 0) return 0;
     if(n <= 1) return n;
 
-    return fib(n-1) + fib(n-2);
+     if(memo[n]) return memo[n];
+
+     memo[n] = fib(n-1, memo) + fib(n-2, memo);
+     return memo[n];
+    
 };
