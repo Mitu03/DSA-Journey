2eabe41d190b79c91bbc2880c390eba4d33a08de
Mitu kr Rabha <166889598+Mitu03@users.noreply.github.com>
2025-08-25 23:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (44.75%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,22 +6,21 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = m - 1, j = n - 1, k = m + n -1;
+  let i = m - 1;        // last valid index in nums1
+  let j = n - 1;        // last index in nums2
+  let k = m + n - 1;    // fill position from the end of nums1
 
-    while(i >= 0 && j >= 0){
-        if(nums1[i] > nums2[j]){
-            nums1[k] = nums1[i];
-            i--;
-        }else{
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
+  // Merge from the back so we don't overwrite nums1's valid part
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
     }
+  }
 
-    while(j >= 0){
-        nums1[k] = nums2[j];
-        j--;
-        k--;
-    }
+  // Only nums2 leftovers need copying (nums1 leftovers already in place)
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
 };
