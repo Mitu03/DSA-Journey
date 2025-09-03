86518ef579958da0f1d2810667879ce8ff311e0f
Mitu kr Rabha <166889598+Mitu03@users.noreply.github.com>
2025-09-03 21:32:46 +0530

    Time: 1 ms (84.39%) | Memory: 55.2 MB (75.47%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -1,24 +1,29 @@
 /**
- * @param {number[]} numbers
- * @param {number} target
- * @return {number[]}
+ * @param {number[]} numbers - sorted array
+ * @param {number} target - target sum
+ * @return {number[]} - 1-indexed positions
  */
-var twoSum = function(numbers, target) {
-    let left = 0;
-    let right = numbers.length - 1;
-    let sum = 0;
+function twoSum(numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
 
-    while(left < right){
-       sum = numbers[left] +  numbers[right];
-       if(sum === target){
-          return [left + 1, right+1];
-       }else if(sum < target){
-          left++;
-       }else{
-        right--;
-       }
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
 
-      
+    if (sum === target) {
+      // Return 1-indexed positions
+      return [left + 1, right + 1];
+    } else if (sum < target) {
+      left++; // Increase sum
+    } else {
+      right--; // Decrease sum
     }
+  }
 
-};
+  return []; // As per problem constraints, there will always be a solution
+}
+
+// Demo usage
+console.log(twoSum([2, 7, 11, 15], 9));  // Output: [1, 2]
+console.log(twoSum([2, 3, 4], 6));       // Output: [1, 3]
+console.log(twoSum([-1, 0], -1));        // Output: [1, 2]
